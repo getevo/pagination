bf0c34497eb22cff95860d3c696cc2cfd769c161
Reza <reza@ies-italia.it>
2025-01-07 00:45:59 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,51 @@
+module githbub.com/getevo/pagination
+
+go 1.22
+
+require (
+	github.com/getevo/evo/v2 v2.0.0-20250106233734-17a82334cb3b
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/alecthomas/repr v0.4.0 // indirect
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
+	github.com/awoodbeck/strftime v0.0.0-20180221155908-016cde65fcde // indirect
+	github.com/getevo/json v0.0.0-20240816130540-f0ea83b195d9 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/gofiber/fiber/v2 v2.52.6 // indirect
+	github.com/gofiber/utils/v2 v2.0.0-beta.7 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/iancoleman/strcase v0.3.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kelindar/binary v1.0.19 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/microsoft/go-mssqldb v1.8.0 // indirect
+	github.com/otiai10/copy v1.14.1 // indirect
+	github.com/otiai10/mint v1.6.3 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.58.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
+	gorm.io/driver/sqlite v1.5.7 // indirect
+	gorm.io/driver/sqlserver v1.5.4 // indirect
+)
---
+++ pagination.go
@@ -0,0 +1,173 @@
+package pagination
+
+import (
+	"github.com/getevo/evo/v2"
+	"github.com/getevo/evo/v2/lib/outcome"
+	"github.com/getevo/evo/v2/lib/ptr"
+	"gorm.io/gorm"
+)
+
+// Pagination represents a utility type for handling pagination in Go.
+//
+// Fields:
+// - Records: Total number of rows.
+// - CurrentPage: Current page loaded.
+// - Pages: Total number of pages.
+// - Limit: Number of rows per page.
+// - First: First page.
+// - Last: Last page.
+// - PageRange: Range of visible pages.
+//
+// Methods:
+// - SetCurrentPage: Sets the current page based on the provided value. If the value is 0, the current page is set to 1.
+// - SetLimit: Sets the limit of rows per page. If the value is 0, the limit is set to the minimum limit of 10. If the limit is less than the minimum limit, it is set to the minimum
+type Pagination struct {
+	Model       *gorm.DB    `json:"-"`
+	Success     bool        `json:"success"`
+	Error       *string     `json:"error,omitempty"`
+	Records     int         `json:"records"`      // Total rows
+	CurrentPage int         `json:"current_page"` // Current Page loaded
+	Pages       int         `json:"pages"`        // total number of pages
+	Size        int         `json:"size"`         // number of rows per page
+	MaxSize     int         `json:"max_size"`
+	First       int         `json:"first"` // First Page
+	Last        int         `json:"last"`  // Last Page
+	Data        interface{} `json:"data"`
+}
+
+// SetCurrentPage sets the value of CurrentPage in the Pagination struct.
+// If the input page is not equal to zero, p.CurrentPage will be set to the input page.
+// Otherwise, p.CurrentPage will be set to 1.
+func (p *Pagination) SetCurrentPage(page int) {
+	if page > 0 {
+		p.CurrentPage = page
+	} else {
+		p.CurrentPage = 1
+	}
+}
+
+func (p *Pagination) SetMaxSize(limit int) {
+	p.MaxSize = limit
+	p.SetSize(p.Size) // Update size if max size has been changed.
+}
+
+// SetSize sets the limit per page for the pagination struct. The limit must be between 10 and 100 (inclusive). If the limit is 0, it will be set to 10. If the limit is less than 10
+func (p *Pagination) SetSize(limit int) {
+	if limit != 0 {
+		p.Size = p.MaxSize
+	} else {
+		p.Size = 1
+	}
+
+	if p.Size < 1 {
+		p.Size = 1
+	} else if p.Size > p.MaxSize {
+		p.Size = p.MaxSize
+	}
+
+}
+
+// SetPages sets the total number of pages in the pagination struct based on the number of records and the limit per page.
+// If the number of records is 0, it sets the number of pages to 1.
+// If there is no remainder when dividing the number of records by the limit, it sets the number of pages to the integer division.
+// Otherwise, it sets the number of pages to the integer division plus 1.
+// If the number of pages is 0, it sets it to 1.
+// After setting the number of pages, it calls the SetLast and SetPageRange methods to update the last page indicator and the range of visible pages respectively.
+func (p *Pagination) SetPages() {
+
+	if p.Records == 0 {
+		p.Pages = 1
+		return
+	}
+
+	res := p.Records % p.Size
+	if res == 0 {
+		p.Pages = p.Records / p.Size
+	} else {
+		p.Pages = (p.Records / p.Size) + 1
+
+	}
+
+	if p.Pages == 0 {
+		p.Pages = 1
+	}
+
+	p.SetLast()
+
+}
+
+// SetLast sets the value of the Last page in the pagination struct.
+// It calculates the value by adding the current offset to the limit.
+// If the calculated value is greater than the total number of records,
+// it sets the Last page to the total number of records.
+func (p *Pagination) SetLast() {
+	p.Last = p.GetOffset() + p.Size
+	if p.Last > p.Records {
+		p.Last = p.Records
+	}
+}
+
+// GetOffset calculates the offset for paginating the data based on the current page and limit
+func (p *Pagination) GetOffset() int {
+	return (p.GetPage() - 1) * p.Size
+}
+
+// GetPage returns the current page of the pagination struct
+func (p *Pagination) GetPage() int {
+	if p.CurrentPage < 1 {
+		p.CurrentPage = 1
+	}
+
+	return p.CurrentPage
+}
+
+func New(model *gorm.DB, request *evo.Request) *Pagination {
+	var p = Pagination{}
+	var limit = request.Query("limit").Int()
+	var page = request.Query("page").Int()
+	if limit < 10 {
+		limit = 10
+	}
+	if page < 1 {
+		page = 1
+	}
+	p.SetCurrentPage(page)
+	p.SetSize(limit)
+
+	return &p
+}
+
+func (p *Pagination) Load(out interface{}) error {
+	var total int64
+	if err := p.Model.Count(&total).Error; err != nil {
+		return err
+	}
+	p.Records = int(total)
+	p.SetPages()
+
+	p.Model = p.Model.Limit(p.Size)
+	p.Model = p.Model.Offset(p.GetOffset())
+	if err := p.Model.Find(out).Error; err != nil {
+		if err != nil {
+			p.Error = ptr.String("unable to load data from the database")
+		}
+		return err
+	}
+	p.Success = true
+	p.Data = out
+	return nil
+}
+
+func (p *Pagination) GetResponse() outcome.Response {
+	var response = outcome.Response{
+		Data: p,
+	}
+	if p.Success {
+		response.Status(200)
+
+	} else {
+		response.Status(500)
+	}
+	response.ContentType = "application/json"
+	return response
+}
